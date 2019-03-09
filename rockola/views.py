a6e9c98e3e373d3cd98924d0d3074ef8bf67be46
Ulises <ulises.torrella@gmail.com>
2019-03-09 13:13:20 -0300

    listing songs

--- rockola/views.py
+++ rockola/views.py
@@ -3,15 +3,25 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
-from interface import *
+from rockola.interface import *
+
+listita = []
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html', {})
+    songs = scan_songs("music/basicsongs")
+    print songs
+    return render(request, 'index.html', {'songs':songs})
+
 def pushSong(request):
+    print "puto el que lee"
     if request.POST:
         #return HttpResponse(request.song);
-        play_song(request.song);
-        return HttpResponse('andando');
+        play_song(request.POST['song'])
+        #Esto deberia ser algo asi supongo
+        #  tmp = Globito(findSongByPath(request.POST.song),request.POST.macAdress);
+        #  listita.append(tmp);
+        return HttpResponse(200)
     else:
-        return HttpResponse('no andando');
+        return HttpResponse(400)
