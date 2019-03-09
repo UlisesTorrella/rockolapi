70a21d274c5792eb18c291cf365749c5e0021571
Ulises <ulises.torrella@gmail.com>
2019-03-09 13:13:20 -0300

    listing songs

--- rockola/views.py
+++ rockola/views.py
@@ -3,7 +3,10 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
+from rockola.interface import *
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html', {})
+    songs = scan_songs("music/basicsongs")
+    print songs
+    return render(request, 'index.html', {'songs':songs})
