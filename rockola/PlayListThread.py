3f03a4106526b03c6f6b7eac11230e987f86269a
Francisco de Maussion <franciscodemaussion@gmail.com>
2019-03-09 16:10:02 -0300

    push to rasp4

--- rockola/PlayListThread.py
+++ rockola/PlayListThread.py
@@ -1,6 +1,8 @@
 from threading import Thread
 
-from rockola import Interface, Queue
+from rockola.Interface import *
+
+import Queue
 
 
 class PlayListThread(Thread):
@@ -9,14 +11,14 @@ class PlayListThread(Thread):
 
     def run(self):
         print "here im 1"
-        songs = Interface.scan_songs("/media/usb")
+        songs = scan_songs("/media/usb")
         print "here im 2"
         while True:
             if Queue.hasNext():
                 print "Hay next"
                 next_blob = Queue.getNext()
                 next_song = next_blob.song
-                Interface.play_song(next_song.path)
+                play_song(next_song.path)
             else:
                 print "Moriremos todos!!!!"
-                Interface.play_random_song(songs)
+                play_random_song(songs)
