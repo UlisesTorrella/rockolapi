667111eb9beb181862ed05022887ab305f4214c8
Francisco de Maussion <franciscodemaussion@gmail.com>
2019-03-09 16:31:04 -0300

    clean

--- rockolapi/wsgi.py
+++ rockolapi/wsgi.py
@@ -15,3 +15,9 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rockolapi.settings")
 
 application = get_wsgi_application()
+
+from rockola.PlayListThread import PlayListThread
+
+cola = PlayListThread()
+cola.setName('Cola')
+cola.start()
