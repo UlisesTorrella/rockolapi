211bbb816358fd070277ae5de9877f8a5380bbff
Ulises <ulises.torrella@gmail.com>
2019-03-09 12:36:32 -0300

    base.html started

--- rockola/views.py
+++ rockola/views.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("no esta hecho")
+    return render(request, 'index.html', {})
