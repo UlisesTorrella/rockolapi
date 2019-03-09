886706f702e0908ce05b0536962ae6b4a4a53df1
Antiloope <rodrigopizarro1234@gmail.com>
2019-03-09 13:11:43 -0300

    Funcion pushSong con algo adentro

--- rockola/views.py
+++ rockola/views.py
@@ -3,9 +3,15 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
+from interface import *
 
 # Create your views here.
 def home(request):
     return render(request, 'index.html', {})
 def pushSong(request):
-    return HttpResponse('casa');
+    if request.POST:
+        #return HttpResponse(request.song);
+        play_song(request.song);
+        return HttpResponse('andando');
+    else:
+        return HttpResponse('no andando');
