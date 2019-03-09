868de7358d459ad196aad6de84c2a302b9dee4d5
Antiloope <rodrigopizarro1234@gmail.com>
2019-03-09 13:11:43 -0300

    Funcion pushSong con algo adentro

--- rockola/urls.py
+++ rockola/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 import rockola.views as views
 
 urlpatterns = [
-    url(r'', views.home),
+    url(r'^$', views.home),
+    url(r'/push_song', views.pushSong,name='pushSong'),
 ]
